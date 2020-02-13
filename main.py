3dd17607fb6b410a240bb2f12a089ec1e3663a85
Stan <spammyboy@protonmail.com>
2020-02-13 00:29:51 +0000

    humble beginnings

---
+++ main.py
@@ -0,0 +1,17 @@
+# Stan O 2020
+# If you are reading this, I want you to do know I made this instead of studying
+
+import random as r
+
+consonants = list("qwrtyplkjhgfdszxcvbnm")
+vowels = list("aeiou")
+
+generatedWord = ""
+
+for i in range( r.randint(3,15) ):
+    if r.randint(1,2) == 1:
+        generatedWord += r.choice(consonants)
+    else:
+        generatedWord += r.choice(vowels)
+
+print(generatedWord)
